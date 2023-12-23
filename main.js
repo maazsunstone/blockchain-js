8b5ce9c7205c92b70c17056a54d45e0d1a30264c
maazsunstone <maaz.abdullah@sunstone.in>
2023-12-24 04:29:38 +0530

    new blocks on blockchain

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
-const SHA256 =  require('crypto-js/sha256')
-class Block{
-    constructor (index, timestamp, data, previousHash=''){
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data =  data;
-        this.previousHash = previousHash;
-        this.hash =''
-
-    }
-    calculateHash(){
-        return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
-    }
+const SHA256 = require("crypto-js/sha256");
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
 }
+
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+  createGenesisBlock() {
+    return new Block(0, "01-01-2024", "Genesis Block", "0");
+  }
+  getLatestBlock(){
+    return this.chain[this.chain.length-1]
+  }
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash()
+    this.chain.push(newBlock)
+  }
+}
+
+
+let sataCoin = new BlockChain()
+sataCoin.addBlock( new Block(1, "10-01-2024", {amount:4}))
+sataCoin.addBlock( new Block(2, "11-01-2024", {amount:10}))
+console.log(JSON.stringify(sataCoin, null, 4))
